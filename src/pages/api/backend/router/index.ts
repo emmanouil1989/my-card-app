dbb6b62717fe165005ba6cd9507a865ff0d80543
emmanoui.e <manos.emmanouil@deliveryblueprints.com>
2022-09-02 18:13:03 +0100

    integrate with mock moonpib card api

--- src/pages/api/backend/router/index.ts
+++ src/pages/api/backend/router/index.ts
@@ -2,18 +2,60 @@ import * as trpc from '@trpc/server';
 import { z } from 'zod';
 export const appRouter = trpc
   .router()
-  .query('hello', {
-    input: z
-      .object({
-        text: z.string().nullish(),
-      })
-      .nullish(),
-    resolve({ input }) {
-        console.log('inside backend router')
+  .query('card-search', {
+
+   async resolve() {
+       const response = await fetch("https://moonpig.github.io/tech-test-frontend/search.json");
+       const jsonResponse = await response.json() as CardSearch;
       return {
-        greeting: `hello ${input?.text ?? 'world'}`,
+       ...jsonResponse
       };
     },
   });
 // export type definition of API
 export type AppRouter = typeof appRouter;
+
+type CardSearch = {
+    SearchId: string;
+    NumberOfProducts: number;
+    Start: number;
+    Products:Array<{
+        price: {
+            Value: number;
+            Currency: string;
+        },
+        SoldOut: number;
+        Title: string;
+        ProductCategory: {
+            ProductCategoryId: number;
+            Name: string;
+        },
+        PhotoUploadCount: number;
+        CardShopId: number;
+        DirectSmile: boolean;
+        DefaultSizeId: number;
+        ProductId: number;
+        MoonpigProductNo: string;
+        TradingFaces: number;
+        IsLandScape: number;
+        ShorDescription: string;
+        Description: string;
+        isCustomable: number ;
+        IsMultiPack: number;
+        SeoPath: string;
+        ProductCategoryGroupSeoPath: string;
+        ProductLink: Link,
+        ProductImage: {
+            Link:Link
+            MimeType: string;
+        }
+
+    }>
+}
+
+type Link = {
+    Href: string;
+    Method: string;
+    Rel: string;
+    Title: string;
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,17 @@ import { trpc } from '../utils/trpc'
 
 const Home: NextPage = () => {
 
-  const {data, isLoading} = trpc.useQuery(["hello",])
+  const {data, isLoading} = trpc.useQuery(["card-search"])
 
   if(isLoading) return <div>Loading...</div>
   return (
     <div className='h-screen w-screen flex justify-center items-center'>
-    <div className=' flex justify-center items-center border-2 border-red-600 border-solid h-10 max-w-md p-10'>
-      {JSON.stringify(data, null, 2)}
+    <div className='pb-10'></div>
+    <div className=' flex justify-center items-center border-2 border-red-600 border-solid  p-10 overflow-auto'>
+      <ul className='max-w-max max-h-96'>
+      {data?.Products.filter(({Title}) => Title.length > 0).map(({Title})=><li>{Title}</li> )}
+      </ul>
+   
     </div>
     </div>
   )
