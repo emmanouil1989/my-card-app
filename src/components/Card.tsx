45457840d60dcff823375fd25e9c2a41e3cc41e3
emmanouil1989 <emmanouil.e25@gmail.com>
2022-09-09 11:44:48 +0100

    improve responsive for mobile

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -7,15 +7,15 @@ type CardProps = PropsWithChildren<{
 }>;
 export default function Card({ data }: CardProps) {
   return (
-    <ul className="grid xl:grid-cols-[300px_300px_300px_300px] lg:grid-cols-[200px_200px_200px] lg:auto-rows-[300px] sm:grid-cols-[300px] sm:grid-auto-rows[400px] md:grid-cols-[200px_200px] md:auto-rows-[300px] xl:auto-rows-[400px] gap-8  h-full">
+    <ul className="grid xl:grid-cols-[300px_300px_300px_300px] lg:grid-cols-[200px_200px_200px] md:grid-cols-[200px_200px] grid-cols-[300px]  xl:auto-rows-[400px] lg:auto-rows-[300px]  md:auto-rows-[300px] auto-rows-[400px]  gap-8  h-full">
       {data.map(({ productId, imageLink, title }) => (
         <li key={productId} className="h-full w-full  flex justify-center">
-          <div className=" h-full w-full relative">
+          <div className=" h-full w-full relative shadow-2xl shadow-neutral-600 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300">
             <Image
               src={imageLink}
               alt={title}
               layout="fill"
-              className="rounded"
+              className="rounded "
             />
           </div>
         </li>
