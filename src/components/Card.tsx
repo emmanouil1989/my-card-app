4a31b20011ceb8f86d3a86719a91caf2261fc1c0
emmanouil1989 <emmanouil.e25@gmail.com>
2022-09-09 23:49:04 +0100

    add search bar

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -7,10 +7,10 @@ type CardProps = PropsWithChildren<{
 }>;
 export default function Card({ data }: CardProps) {
   return (
-    <ul className="grid xl:grid-cols-[300px_300px_300px_300px] lg:grid-cols-[200px_200px_200px] md:grid-cols-[200px_200px] grid-cols-[300px]  xl:auto-rows-[400px] lg:auto-rows-[300px]  md:auto-rows-[300px] auto-rows-[400px]  gap-8  h-full">
+    <ul className="grid xl:grid-cols-[300px_300px_300px_300px] lg:grid-cols-[200px_200px_200px] md:grid-cols-[200px_200px] grid-cols-[300px]  xl:auto-rows-[400px] lg:auto-rows-[300px]  md:auto-rows-[300px] auto-rows-[400px]  gap-8  h-full ">
       {data.map(({ productId, imageLink, title }) => (
         <li key={productId} className="h-full w-full  flex justify-center">
-          <div className=" h-full w-full relative shadow-2xl shadow-neutral-600 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300">
+          <div className=" h-full w-full relative shadow-md shadow-yellow-100 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
             <Image
               src={imageLink}
               alt={title}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Card from "@/components/Card";
 import type { NextPage } from "next";
 import { trpc } from "../utils/trpc";
+import Image from "next/image";
 
 const Home: NextPage = () => {
   const { data, isLoading, isSuccess } = trpc.useQuery(["card-search"]);
@@ -8,9 +9,41 @@ const Home: NextPage = () => {
   const isLoadingOrError = isLoading || !isSuccess;
 
   return (
-    <main className="h-screen w-screen flex pt-12 pb-12">
-      <div className=" flex justify-center overflow-y-auto w-full max-h-fit items-center">
-        {isLoadingOrError ? <img src={"/ball-triangle.svg"} alt={'loading indicator'} className={"w-56 h-56"}/> : <Card data={data!.cards}  />}
+    <main className=" w-screen flex  pb-12">
+      <div className=" flex flex-col justify-center overflow-y-auto w-full  items-center">
+        <div className="m-w-[1200px]">
+          <div className="flex justify-between items-center py-8 w-full">
+            <h2>CardPool</h2>
+            <div className="w-8/12 h-10 relative">
+              <input
+                className=" rounded-3xl p-4 border-solid  bg-slate-600 w-full h-11 outline-none border-2 border-gray-300 font-bold placeholder:text-white placeholder:font-bold"
+                placeholder="Search for cards..."
+              />
+              <svg
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 28 28"
+                width="28px"
+                height="28px"
+                className="absolute top-2 right-4 bottom-2"
+              >
+                <path
+                  d="M11 2a9 9 0 1 0 3.88 17.123l5.777 5.777a3 3 0 0 0 4.243-4.243l-5.777-5.777A9 9 0 0 0 11 2zm-7 9a7 7 0 1 1 14 0 7 7 0 0 1-14 0zm14.032 5.618a9.054 9.054 0 0 1-1.414 1.414l5.453 5.453a1 1 0 0 0 1.414-1.414z"
+                  fill="currentColor"
+                ></path>
+              </svg>
+            </div>
+          </div>
+          {isLoadingOrError ? (
+            <img
+              src={"/ball-triangle.svg"}
+              alt={"loading indicator"}
+              className={"w-56 h-56"}
+            />
+          ) : (
+            <Card data={data!.cards} />
+          )}
+        </div>
       </div>
     </main>
   );
