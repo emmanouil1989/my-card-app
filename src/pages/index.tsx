92fae55e47c0b96e0b4f33ab91af29f11effecbd
emmanouil1989 <emmanouil.e25@gmail.com>
2022-09-10 09:53:52 +0100

    responsive changes of header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ const Home: NextPage = () => {
 
   return (
     <main className=" h-screen w-screen flex flex-col justify-between items-center relative">
-      <div className=" flex flex-col justify-center  w-full  items-center">
+      <div className=" flex flex-col justify-center  w-full  items-center ">
         {isLoadingOrError ? (
           <img
             src={"/ball-triangle.svg"}
@@ -19,9 +19,9 @@ const Home: NextPage = () => {
           />
         ) : (
           <div>
-            <div className="flex justify-between items-center py-8 w-full">
+            <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center py-8 w-full">
               <h2>CardPool</h2>
-              <div className="w-8/12 h-10 relative">
+              <div className="sm:w-8/12 w-[19rem] h-10 relative py-4 sm:py-0 ">
                 <input
                   className=" rounded-3xl p-4 border-solid  bg-slate-600 w-full h-11 outline-none border-2 border-gray-300 font-bold placeholder:text-white placeholder:font-bold"
                   placeholder="Search for cards..."
@@ -32,7 +32,7 @@ const Home: NextPage = () => {
                   viewBox="0 0 28 28"
                   width="28px"
                   height="28px"
-                  className="absolute top-2 right-4 bottom-2"
+                  className="absolute top-6 sm:top-2 right-4 bottom-2"
                 >
                   <path
                     d="M11 2a9 9 0 1 0 3.88 17.123l5.777 5.777a3 3 0 0 0 4.243-4.243l-5.777-5.777A9 9 0 0 0 11 2zm-7 9a7 7 0 1 1 14 0 7 7 0 0 1-14 0zm14.032 5.618a9.054 9.054 0 0 1-1.414 1.414l5.453 5.453a1 1 0 0 0 1.414-1.414z"
