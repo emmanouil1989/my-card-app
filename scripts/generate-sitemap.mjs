d80a27166e386a25a019bd08a1b3cdc747d861e3
emmanouil1989 <emmanouil.e25@gmail.com>
2022-09-13 10:20:48 +0100

    move generate sitemap to js

---
+++ scripts/generate-sitemap.mjs
@@ -0,0 +1,62 @@
+import { writeFileSync } from 'fs';
+import prettier from 'prettier';
+
+
+const baseUrl = {
+    production: 'https://theblackhart.co.uk',
+    preview: process.env.VERCEL_URL,
+    development: 'http://localhost:3000',
+  }
+
+async function generate() {
+  const prettierConfig = await prettier.resolveConfig('./.prettierrc.json');
+  (async () => {
+    const { globby } = await import('globby')
+    const pages =  await globby([
+        'pages/*.tsx',
+        'data/**/*.mdx',
+        '!data/*.mdx',
+        '!pages/_*.tsx',
+        '!pages/api',
+        '!pages/404.tsx'
+      ])
+      console.log({pages})
+      const sitemap = `
+      <?xml version="1.0" encoding="UTF-8"?>
+      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+          ${pages
+            .map((page) => {
+              const path = page
+                .replace('pages', '')
+                .replace('data', '')
+                .replace('.ts', '')
+                .replace('tsx', '')
+                .replace('.mdx', '');
+              const route = path === '/index' ? '' : path;
+  
+              return `
+                <url>
+                    <loc>${`baseUrl${route}`}</loc>
+                </url>
+              `;
+            })
+            .join('')}
+      </urlset>
+      `;
+  
+    const formatted = prettier.format(sitemap, {
+      ...prettierConfig,
+      parser: 'html'
+    });
+  
+    // eslint-disable-next-line no-sync
+    writeFileSync('public/sitemap.xml', formatted);
+  
+  })()
+
+  
+
+
+}
+
+generate();
