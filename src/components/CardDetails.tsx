2d070f9465bc7b8253e55d2cc6f06e3f2a2b6ae9
emmanouil1989 <emmanouil.e25@gmail.com>
2022-09-16 20:53:34 +0100

    styles added to card details for mobile screens

--- src/components/CardDetails.tsx
+++ src/components/CardDetails.tsx
@@ -12,29 +12,38 @@ export default function CardDetails() {
     { cardId },
   ]);
 
-  if (isLoading || !isSuccess) {
-    return <LoadingIndicator />;
-  }
-  const card = data.card;
+  const isLoadingOrError = isLoading || !isSuccess;
 
   return (
     <div
-      className={`flex flex-col sm:flex-row sm:justify-center h-full w-full  items-center pt-10 sm:pt-0`}
+      className={`flex flex-col sm:flex-row ${
+        isLoading ? "justify-center" : "justify-start"
+      } h-full w-full  items-center pt-10 sm:pt-0`}
     >
-      <div className="relative shadow-lg shadow-yellow-100 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 aspect-w-1 aspect-h-1 overflow-hidden rounded-lg  xl:aspect-w-7 xl:aspect-h-8 w-[400px] h-[400px]">
-        <Image src={card.imageLink} alt={card.title} layout="fill" />
-      </div>
-      <div className="py-4" />
-      <div className="flex flex-col p-4">
-        <h1 className="text-2xl font-bold pb-4">{card.title}</h1>
-        <p className="text-lg">
-          {card.description.replace(/(<([^>]+)>)/gi, "")}
-        </p>
-        <button className="bg-yellow-500 text-white p-2 rounded-lg mt-4 font-bold text-lg">
-          {card.price}
-          {card.currency} Buy Now
-        </button>
-      </div>
+      {isLoading || !isSuccess ? (
+        <LoadingIndicator />
+      ) : (
+        <>
+          <div className="relative shadow-lg shadow-yellow-100 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 aspect-w-1 aspect-h-1 overflow-hidden rounded-lg  xl:aspect-w-7 xl:aspect-h-8 w-[400px] h-[400px]">
+            <Image
+              src={data.card.imageLink}
+              alt={data.card.title}
+              layout="fill"
+            />
+          </div>
+          <div className="py-4" />
+          <div className="flex flex-col p-4">
+            <h1 className="text-2xl font-bold pb-4">{data.card.title}</h1>
+            <p className="text-lg">
+              {data.card.description.replace(/(<([^>]+)>)/gi, "")}
+            </p>
+            <button className="bg-yellow-500 text-white p-2 rounded-lg mt-4 font-bold text-lg">
+              {data.card.price}
+              {data.card.currency} Buy Now
+            </button>
+          </div>
+        </>
+      )}
     </div>
   );
 }
