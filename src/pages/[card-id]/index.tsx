ba1450601003f5f3ae654d650c83452b74801fe8
emmanouil1989 <emmanouil.e25@gmail.com>
2022-09-17 12:15:26 +0100

    add head to details page

--- src/pages/[card-id]/index.tsx
+++ src/pages/[card-id]/index.tsx
@@ -1,9 +1,16 @@
 import CardDetails from '@/components/CardDetails';
+import Head from 'next/head';
 import React from 'react';
 
 
 
 
 export default function CardDetailPage() {
-  return <CardDetails />;
+  return <>
+      <Head>
+        <title>Cards Pool App</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+    <CardDetails />
+  </>
 }
