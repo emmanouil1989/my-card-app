01bb987426612b81d083074a4cdbc6ededed9b92
emmanouil1989 <emmanouil.e25@gmail.com>
2022-09-19 16:56:15 +0100

    fix trpc configuration

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,8 +1,9 @@
 import * as trpcNext from '@trpc/server/adapters/next';
 import {appRouter} from '../backend/routers/appRouter';
 import { withCors } from '@/utils/network';
+import { createContext } from '../backend/context';
 
 export default withCors(trpcNext.createNextApiHandler({
     router: appRouter,
-    createContext: () => null,
+    createContext,
   }));
