4d44678402bef9915e9a2104562ef45387c0edfe
emmanouil1989 <emmanouil.e25@gmail.com>
2022-09-22 11:53:45 +0100

    change overflow x on card list

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -13,7 +13,7 @@ export default function CardList({ data }: CardListProps) {
   };
   return (
     <div className="flex h-4/5 pr-4">
-      <ul className="grid xl:grid-cols-[300px_300px_300px_300px] lg:grid-cols-[200px_200px_200px] md:grid-cols-[200px_200px] grid-cols-[300px]  xl:auto-rows-[400px] lg:auto-rows-[300px]  md:auto-rows-[300px] auto-rows-[400px] list-inside gap-8  h-full overflow-y-auto pr-4">
+      <ul className="grid xl:grid-cols-[300px_300px_300px_300px] lg:grid-cols-[200px_200px_200px] md:grid-cols-[200px_200px] grid-cols-[300px]  xl:auto-rows-[400px] lg:auto-rows-[300px]  md:auto-rows-[300px] auto-rows-[400px] list-inside gap-8  h-full overflow-y-auto overflow-x-hidden pr-4">
         {data.map(({ productId, imageLink, title }) => (
           <li key={productId} className="h-full w-full  flex justify-center">
             <div className=" h-full w-full relative shadow-lg shadow-yellow-100 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 aspect-w-1 aspect-h-1 overflow-hidden rounded-lg  xl:aspect-w-7 xl:aspect-h-8">
