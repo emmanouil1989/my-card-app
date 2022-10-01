0af59214f53462471e921d39d84ba36343e63dcb
emmanouil1989 <emmanouil.e25@gmail.com>
2022-10-01 14:33:39 +0100

    test action

--- tests/test-card-details.spec.ts
+++ tests/test-card-details.spec.ts
@@ -1,13 +1,13 @@
 import { test, expect } from '@playwright/test';
 
-test('test card details page', async ({ page }) => {
+test('test card details page', async ({ page, baseURL }) => {
 
   // Go to http://localhost:3000/
-  await page.goto('/');
+  await page.goto(baseURL);
 
   // Click li:nth-child(2) > div > .relative
   await page.locator('li:nth-child(2) > div > .relative').click();
-  await expect(page).toHaveURL('/bsk001');
+  await expect(page).toHaveURL(`${baseURL}/bsk001`);
   await expect(page.locator('img[alt="Baby Shark song photo upload kids Birthday card"]')).toBeVisible();
   await expect(page.locator('text=3.49£ Buy Now')).toBeVisible();
   await expect (page.locator('text=3.49£ Buy Now')).toBeVisible();
