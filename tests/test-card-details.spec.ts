6bf5c1fdfcf8d8bed203bf0c2add67b50c9130f6
emmanouil1989 <emmanouil.e25@gmail.com>
2022-10-01 13:56:59 +0100

    test github action

--- tests/test-card-details.spec.ts
+++ tests/test-card-details.spec.ts
@@ -3,11 +3,11 @@ import { test, expect } from '@playwright/test';
 test('test card details page', async ({ page }) => {
 
   // Go to http://localhost:3000/
-  await page.goto('http://localhost:3000/');
+  await page.goto('/');
 
   // Click li:nth-child(2) > div > .relative
   await page.locator('li:nth-child(2) > div > .relative').click();
-  await expect(page).toHaveURL('http://localhost:3000/bsk001');
+  await expect(page).toHaveURL('/bsk001');
   await expect(page.locator('img[alt="Baby Shark song photo upload kids Birthday card"]')).toBeVisible();
   await expect(page.locator('text=3.49£ Buy Now')).toBeVisible();
   await expect (page.locator('text=3.49£ Buy Now')).toBeVisible();
--- tests/test-landing-page.spec.ts
+++ tests/test-landing-page.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 test('Landing Page', async ({ page }) => {
 
   // Go to http://localhost:3000/ 
-  await page.goto('http://localhost:3000/');
+  await page.goto('/');
   await expect(page.locator('button >> nth=0')).toBeVisible();
   await expect(page.locator('div:has-text("CardPool") >> nth=2')).toBeVisible();
   await expect(page.locator('text=12345...20')).toBeVisible();
