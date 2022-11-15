b4c45b5d8af92a2de0cd96c31a7431fc03a86f46
emmanouil1989 <emmanouil.e25@gmail.com>
2022-11-15 08:35:37 +0000

    change seed file

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -1,71 +1,71 @@
-import { Prisma, PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
-import axios from 'axios';
+import { Prisma, PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+import axios from "axios";
 type CardSearch = {
-    SearchId: string;
-    NumberOfProducts: number;
-    Start: number;
-    Products:Array<{
-        Price: {
-            Value: number;
-            Currency: string;
-        },
-        SoldOut?: number;
-        Title: string;
-        ProductCategory: {
-            ProductCategoryId: number;
-            Name: string;
-        },
-        PhotoUploadCount: number;
-        CardShopId: number;
-        DirectSmile: boolean;
-        DefaultSizeId: number;
-        ProductId: number;
-        MoonpigProductNo: string;
-        TradingFaces: number;
-        IsLandScape: number;
-        ShorDescription: string;
-        Description: string;
-        isCustomable: number ;
-        IsMultiPack: number;
-        SeoPath: string;
-        ProductCategoryGroupSeoPath: string;
-        ProductLink: Link,
-        ProductImage:{
-            Link:Link
-            MimeType: string;
-        }
-
-    }>
-}
+  SearchId: string;
+  NumberOfProducts: number;
+  Start: number;
+  Products: Array<{
+    Price: {
+      Value: number;
+      Currency: string;
+    };
+    SoldOut?: number;
+    Title: string;
+    ProductCategory: {
+      ProductCategoryId: number;
+      Name: string;
+    };
+    PhotoUploadCount: number;
+    CardShopId: number;
+    DirectSmile: boolean;
+    DefaultSizeId: number;
+    ProductId: number;
+    MoonpigProductNo: string;
+    TradingFaces: number;
+    IsLandScape: number;
+    ShorDescription: string;
+    Description: string;
+    isCustomable: number;
+    IsMultiPack: number;
+    SeoPath: string;
+    ProductCategoryGroupSeoPath: string;
+    ProductLink: Link;
+    ProductImage: {
+      Link: Link;
+      MimeType: string;
+    };
+  }>;
+};
 
 type Link = {
-    Href: string;
-    Method: string;
-    Rel: string;
-    Title: string;
-}
+  Href: string;
+  Method: string;
+  Rel: string;
+  Title: string;
+};
 export const fillDB = async () => {
-    const response = await axios.get<CardSearch>("https://moonpig.github.io/tech-test-frontend/search.json");
+  const response = await axios.get<CardSearch>(
+    "https://moonpig.github.io/tech-test-frontend/search.json"
+  );
 
-    try{
-        const mappedData:Array<Prisma.CardCreateManyInput> = response.data.Products.map((product) => ({
-            title: product.Title,
-            description: product.Description,
-            price: product.Price.Value.toString(),
-            currency: product.Price.Currency,
-            productId: product.MoonpigProductNo,
-            imageLink:product.ProductImage.Link.Href,
-        }))
-       const creation = await prisma.card.createMany({
-            data: mappedData
-       });
-    }catch(e){
-        console.log(e, "error");
-    }
-
-
-
-}
+  try {
+    const mappedData: Array<Prisma.CardCreateManyInput> =
+      response.data.Products.map((product) => ({
+        title: product.Title,
+        description: product.Description,
+        price: product.Price.Value.toString(),
+        currency: product.Price.Currency,
+        productId: product.MoonpigProductNo,
+        imageLink: product.ProductImage.Link.Href,
+      }));
+    await prisma.card.deleteMany({});
+    await prisma.card.createMany({
+      data: mappedData,
+    });
+  } catch (e) {
+    console.log(e, "error");
+  }
+};
 
 fillDB();
